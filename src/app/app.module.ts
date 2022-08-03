1e9fb73b0f487d64060f427586485e9ff1e91880
Tom Norton <tmnrtn@gmail.com>
2022-08-03 21:42:15 +0100

    v0.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { HttpClientModule } from '@angular/common/http';
 import {MatTableModule} from '@angular/material/table'
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatInputModule } from '@angular/material/input';
+import { MatSortModule } from '@angular/material/sort';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
 
 import { AppComponent } from './app.component';
 import { GroceriesComponent } from './groceries/groceries.component';
@@ -27,7 +30,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserAnimationsModule,
     MatTableModule,
     MatAutocompleteModule,
-    MatInputModule
+    MatInputModule,
+    MatSortModule,
+    MatButtonModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/groceries/groceries.component.ts
+++ src/app/groceries/groceries.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewChild, AfterViewInit } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
 import {
   debounceTime, distinctUntilChanged, switchMap
 } from 'rxjs/operators';
+import {LiveAnnouncer} from '@angular/cdk/a11y';
+import {MatSort, Sort} from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import {FormControl, ReactiveFormsModule } from '@angular/forms';
 
@@ -26,9 +28,11 @@ export class GroceriesComponent implements OnInit {
   columnsToDisplay = ['name', 'aisle', 'purchased', 'delete'];
   dataSource = new MatTableDataSource<Grocery>();
 
+  @ViewChild(MatSort) sort!: MatSort;
+
   myControl = new FormControl('');
 
-
+  selected = 'Not purchased'
 
   getGroceries(): void {
     this.groceryService.getGroceries()
@@ -68,10 +72,37 @@ export class GroceriesComponent implements OnInit {
     return grocery && grocery.name ? grocery.name[0] : '';
   }
 
-  constructor(private groceryService: GroceryService) { }
+  filterTable():void {
+    this.dataSource.filter = this.selected;
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+  }
+
+    /** Announce the change in sort state for assistive technology. */
+    announceSortChange(sortState: Sort) {
+      // This example uses English messages. If your application supports
+      // multiple language, you would internationalize these strings.
+      // Furthermore, you can customize the message to add additional
+      // details about the values being sorted.
+      if (sortState.direction) {
+        this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+      } else {
+        this._liveAnnouncer.announce('Sorting cleared');
+      }
+    }
+
+  constructor(private groceryService: GroceryService, private _liveAnnouncer: LiveAnnouncer) { }
 
   ngOnInit(): void {
     this.getGroceries();
+
+    this.dataSource.filterPredicate = (data: Grocery, filter: string) => {
+      return data.purchased == (filter == "Purchased") || filter == "All";
+     };
+
+     this.filterTable();
 
     this.searchRes$ = this.searchTerms.pipe(
       // wait 300ms after each keystroke before considering the term
